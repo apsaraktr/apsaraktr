e63e598e50bfede928db96f5e421972d093358f7
apsaraktr <muthukumardharani77@gmail.com>
2024-11-07 18:09:27 +0530

    Day3 completed

---
+++ If condition 3 vote age.py
@@ -0,0 +1,13 @@
+age=int(input("enter the age:"))
+if(age>0):
+	if(age==18) or(age>18):
+		print("elligible for voting")
+	else:
+		print('not eligible for voting')
+else:
+	print('invalid age')
+output
+enter the age:67
+elligible for voting
+
+
