932dee496c18bbadce66a4a75a2d6ddc71a3fbf2
apsaraktr <muthukumardharani77@gmail.com>
2024-11-07 17:58:20 +0530

    Day 3completed

---
+++ If condition  2mark.py
@@ -0,0 +1,14 @@
+mark =int(input("entrr the mark:"))
+if(mark<=100):
+	if(mark>=35):
+		print("pass")
+	else:
+		print("fail")
+else:
+	print("invalid mark")
+
+ output
+ entrr the mark:78
+pass
+
+
