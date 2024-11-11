bf52c5a6bdef3f94d1614b10e2ff78ae04a61f74
apsaraktr <muthukumardharani77@gmail.com>
2024-11-11 22:51:41 +0530

    Create Biggest no.py

---
+++ Biggest no.py
@@ -0,0 +1,16 @@
+a=int(input('enter the 1st no:'))
+b=int(input('enter the 2nd no:'))
+if(a>b):
+	print('a is the bisgest no')
+elif(a==b):
+	print(' a and b are equal')
+else:
+	print('b is the biggest no')
+
+output
+
+enter the 1st no:23
+enter the 2nd no:87
+b is the biggest no
+
+
